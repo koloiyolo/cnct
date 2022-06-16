0df12859eac017033c6c14c6071194b01218d6a5
Jakub Kolodziej <jakvb.kolodziej@gmail.com>
2022-06-17 00:08:48 +0200

    pls work

--- cnct.cpp
+++ cnct.cpp
@@ -3,6 +3,36 @@
 #include <iostream>
 using namespace std;
 
+class Functions
+{
+    public:
+    void showConnections(){
+    }
+    void connect(int i){
+    }
+};
+
+void menu(){
+    Functions f;
+    int choice;
+    int tmp;
+    cout<<"Choice: ";
+    cin>>choice;
+    switch(choice){
+        default:
+        break;
+        case 1:
+        f.showConnections();
+        cout<<"Which server you want to connect to?"<<endl;
+        cout<<"Please enter number: ";
+        cin>>tmp;
+        f.connect(tmp);
+        break;
+    }
+}
+
+
 int main(){
+    menu();
     return 0;
 }
