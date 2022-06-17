2911e9659a2c44f64737a42850e7b4c9394b608a
Jakub Kolodziej <jakvb.kolodziej@gmail.com>
2022-06-17 10:49:29 +0200

    key list working

--- cnct.cpp
+++ cnct.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
 struct cdata
 {
@@ -17,6 +18,7 @@ class Functions
      
     vector<cdata> connections; 
     public:
+    
     void des(){
         fstream data;
         string name, ip, user, key;
@@ -29,12 +31,14 @@ class Functions
             cout<<"Data file doesn't exist"<<endl;
         }
     }
+    
     void showConnections(){
         int i = 0;
         for(auto c:connections){
             cout<<++i<<". "<<c.name<<" "<<c.ip<<endl;
         }
     }
+    
     void connect(int i){
         string command ="ssh ";
         string key="-i /.keys"+connections[i].key+" ";
@@ -47,10 +51,17 @@ class Functions
         }
         system(command.c_str());
     }
+
     void keyList(){
-        system("cd .keys");
-        system("ls -l");
+        //system("cd .keys");
+        //system("ls -l");
+        string path = ".keys";
+        for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
     }
+
+
+    void keysToDir(){}
 };
 
 void menu(){
@@ -58,7 +69,7 @@ void menu(){
     f.des();
     int choice;
     int tmp;
-    cout<<"1. Connect to a server\n2. Connection list\n3. Key list\n4. Add key to keys folder\n5. Add connection\n";
+    cout<<"1. Connect to a server\n2. Connection list\n3. Key list\n4. Add key to keys directory\n5. Add connection\n";
     cout<<"Choice: ";
     cin>>choice;
     switch(choice){
@@ -77,6 +88,9 @@ void menu(){
         break;
         case 3:
             f.keyList();
+        break;
+        case 4:
+            system("mkdir a");
         break;
 
     }
